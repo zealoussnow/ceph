88f82d839d8509ea657d60e70b89c7e78c496164
wanghongxu <wang19930902@gmail.com>
2018-05-30 09:45:26 +0800

    fix kfifo

--- src/os/bluestore/cache/kfifo.c
+++ src/os/bluestore/cache/kfifo.c
@@ -162,3 +162,111 @@ unsigned int __kfifo_out(struct __kfifo *fifo,
   fifo->out += len;
   return len;
 }
+
+unsigned int __kfifo_max_r(unsigned int len, size_t recsize) {
+  unsigned int max = (1 << (recsize << 3)) - 1;
+
+  if (len > max)
+    return max;
+  return len;
+}
+
+
+#define  __KFIFO_PEEK(data, out, mask) \
+  ((data)[(out) & (mask)])
+
+/*
+ * __kfifo_peek_n internal helper function for determinate the length of
+ * the next record in the fifo
+ */
+static unsigned int __kfifo_peek_n(struct __kfifo *fifo, size_t recsize) {
+  unsigned int l;
+  unsigned int mask = fifo->mask;
+  unsigned char *data = fifo->data;
+
+  l = __KFIFO_PEEK(data, fifo->out, mask);
+
+  if (--recsize)
+    l |= __KFIFO_PEEK(data, fifo->out + 1, mask) << 8;
+
+  return l;
+}
+
+#define  __KFIFO_POKE(data, in, mask, val) \
+  ( \
+  (data)[(in) & (mask)] = (unsigned char)(val) \
+  )
+
+/*
+ * __kfifo_poke_n internal helper function for storeing the length of
+ * the record into the fifo
+ */
+static void __kfifo_poke_n(struct __kfifo *fifo, unsigned int n, size_t recsize) {
+  unsigned int mask = fifo->mask;
+  unsigned char *data = fifo->data;
+
+  __KFIFO_POKE(data, fifo->in, mask, n);
+
+  if (recsize > 1)
+    __KFIFO_POKE(data, fifo->in + 1, mask, n >> 8);
+}
+
+unsigned int __kfifo_len_r(struct __kfifo *fifo, size_t recsize) {
+  return __kfifo_peek_n(fifo, recsize);
+}
+
+
+unsigned int __kfifo_in_r(struct __kfifo *fifo, const void *buf,
+                          unsigned int len, size_t recsize) {
+  if (len + recsize > kfifo_unused(fifo))
+    return 0;
+
+  __kfifo_poke_n(fifo, len, recsize);
+
+  kfifo_copy_in(fifo, buf, len, fifo->in + recsize);
+  fifo->in += len + recsize;
+  return len;
+}
+
+
+static unsigned int kfifo_out_copy_r(struct __kfifo *fifo,
+                                     void *buf, unsigned int len, size_t recsize, unsigned int *n) {
+  *n = __kfifo_peek_n(fifo, recsize);
+
+  if (len > *n)
+    len = *n;
+
+  kfifo_copy_out(fifo, buf, len, fifo->out + recsize);
+  return len;
+}
+
+unsigned int __kfifo_out_peek_r(struct __kfifo *fifo, void *buf,
+                                unsigned int len, size_t recsize) {
+  unsigned int n;
+
+  if (fifo->in == fifo->out)
+    return 0;
+
+  return kfifo_out_copy_r(fifo, buf, len, recsize, &n);
+}
+
+
+unsigned int __kfifo_out_r(struct __kfifo *fifo, void *buf,
+                           unsigned int len, size_t recsize) {
+  unsigned int n;
+
+  if (fifo->in == fifo->out)
+    return 0;
+
+  len = kfifo_out_copy_r(fifo, buf, len, recsize, &n);
+  fifo->out += n + recsize;
+  return len;
+}
+
+
+void __kfifo_skip_r(struct __kfifo *fifo, size_t recsize) {
+  unsigned int n;
+
+  n = __kfifo_peek_n(fifo, recsize);
+  fifo->out += n + recsize;
+}
