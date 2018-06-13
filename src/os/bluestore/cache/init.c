d126d89d810765ba685d1b34bd48cd1da5ee8421
wujian <wujian@t2cloud.net>
2018-06-13 10:28:48 -0700

    add log

--- src/os/bluestore/cache/init.c
+++ src/os/bluestore/cache/init.c
@@ -1929,6 +1929,7 @@ aio_read_completion(struct ring_item *item)
       _write_cache_miss(item);
       return;
     } else {
+      CACHE_WARNLOG(NULL,"cache should not hits by strategy \n");
       pthread_rwlock_unlock(&ca->set->dc->writeback_lock);
     }
   }
