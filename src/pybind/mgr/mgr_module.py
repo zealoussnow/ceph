cace54f6a828bb88cad1c65ddc1dac33e485aeee
wujian <wujian@t2cloud.net>
2019-06-14 07:50:39 +0800

    add lat to mgr

--- src/pybind/mgr/mgr_module.py
+++ src/pybind/mgr/mgr_module.py
@@ -542,6 +542,94 @@ class MgrModule(ceph_module.BaseMgrModule):
         else:
             return 0
 
+    def get_osd_lat(self, daemon_type, daemon_name, stat):
+        data = self.get_counter(daemon_type, daemon_name, stat)[stat]
+
+        #self.log.error("get_latest {0} data={1}".format(stat, data))
+        if data and len(data) > 1:
+            count = data[-1][2] - data[-2][2]
+            if not count:
+                return 0
+            return (data[-1][1] - data[-2][1]) / float(count)
+        else:
+            return 0
+
+    def get_osd_lat_sum(self, daemon_type, daemon_name, stat):
+        data = self.get_counter(daemon_type, daemon_name, stat)[stat]
+
+        #self.log.error("get_latest {0} data={1}".format(stat, data))
+        if data and len(data) > 1:
+            return data[-1][1] - data[-2][1]
+        else:
+            return 0
+
+    def get_osd_count_sum(self, daemon_type, daemon_name, stat):
+        data = self.get_counter(daemon_type, daemon_name, stat)[stat]
+
+        #self.log.error("get_latest {0} data={1}".format(stat, data))
+        if data and len(data) > 1:
+            return data[-1][2] - data[-2][2]
+        else:
+            return 0
+
+    def crush_rule_osds(self, nodes, rule):
+        nodes_by_id = dict((n['id'], n) for n in nodes)
+    
+        def _gather_leaf_ids(node):
+            if node['id'] >= 0:
+                return set([node['id']])
+    
+            result = set()
+            for child_id in node['children']:
+                if child_id >= 0:
+                    result.add(child_id)
+                else:
+                    result |= _gather_leaf_ids(nodes_by_id[child_id])
+    
+            return result
+    
+        def _gather_descendent_ids(node, typ):
+            result = set()
+            for child_id in node['children']:
+                child_node = nodes_by_id[child_id]
+                if child_node['type'] == typ:
+                    result.add(child_node['id'])
+                elif 'children' in child_node:
+                    result |= _gather_descendent_ids(child_node, typ)
+    
+            return result
+    
+        def _gather_osds(root, steps):
+            if root['id'] >= 0:
+                return set([root['id']])
+    
+            osds = set()
+            step = steps[0]
+            if step['op'] == 'choose_firstn':
+                # Choose all descendents of the current node of type 'type'
+                d = _gather_descendent_ids(root, step['type'])
+                for desc_node in [nodes_by_id[i] for i in d]:
+                    osds |= _gather_osds(desc_node, steps[1:])
+            elif step['op'] == 'chooseleaf_firstn':
+                # Choose all descendents of the current node of type 'type',
+                # and select all leaves beneath those
+                for desc_node in [nodes_by_id[i] for i in _gather_descendent_ids(root, step['type'])]:
+                    # Short circuit another iteration to find the emit
+                    # and assume anything we've done a chooseleaf on
+                    # is going to be part of the selected set of osds
+                    osds |= _gather_leaf_ids(desc_node)
+            elif step['op'] == 'emit':
+                if root['id'] >= 0:
+                    osds |= root['id']
+    
+            return osds
+    
+        osds = set()
+        for i, step in enumerate(rule['steps']):
+            if step['op'] == 'take':
+                osds |= _gather_osds(nodes_by_id[step['item']], rule['steps'][i + 1:])
+        return osds
+
     def get_osdmap(self):
         """
         Get a handle to an OSDMap.  If epoch==0, get a handle for the latest
