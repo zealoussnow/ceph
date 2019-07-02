6e9e6e8393800840b4efbdd7a0cd75629beba871
Leo Zhang <nguzcf@gmail.com>
2019-07-02 10:52:45 +0800

    mon: add 'size' arg to `osd pool create`

--- src/mon/MonCommands.h
+++ src/mon/MonCommands.h
@@ -921,7 +921,8 @@ COMMAND("osd pool create " \
         "name=pool_type,type=CephChoices,strings=replicated|erasure,req=false " \
 	"name=erasure_code_profile,type=CephString,req=false,goodchars=[A-Za-z0-9-_.] " \
 	"name=rule,type=CephString,req=false " \
-        "name=expected_num_objects,type=CephInt,req=false", \
+        "name=expected_num_objects,type=CephInt,req=false " \
+        "name=size,type=CephInt,req=false", \
 	"create pool", "osd", "rw", "cli,rest")
 COMMAND("osd pool delete " \
 	"name=pool,type=CephPoolname " \
